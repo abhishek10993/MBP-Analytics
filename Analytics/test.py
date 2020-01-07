070a0c37979e774a85bc6dc2823b27f38cf26c42
abhishek10993 <abhishek.10993@gmail.com>
2020-01-07 10:23:30 +0100

    Added code to download data

--- Analytics/test.py
+++
@@ -1,5 +0,0 @@
-import configparser
-config = configparser.RawConfigParser()
-config.read('../resources/misc.properties')
-
-print (config.get('MBP', 'database.dbname'))
--- REST_Controllers/index.py
+++ REST_Controllers/index.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
+	print(request.args.get('sensor'))
+	print(request.args.get('algo'))
 	return "Hello, World!"
