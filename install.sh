f9506ead657fc749628277fe11a0cfefb80f24ec
abhishek10993 <abhishek.10993@gmail.comm>
2020-04-15 10:04:58 -0700

    updated install script

--- install.sh
+++ install.sh
@@ -24,14 +24,27 @@ wget https://downloads.apache.org/spark/spark-3.0.0-preview2/spark-3.0.0-preview
 mkdir spark;
 tar -C spark -xvf spark-3.0.0-preview2-bin-hadoop2.7.tgz;
 export SPARK_HOME="$PWD/spark/spark-3.0.0-preview2-bin-hadoop2.7";
+echo $SPARK_HOME;
 wget https://github.com/jpmml/jpmml-sparkml/releases/download/1.6.0/jpmml-sparkml-executable-1.6.0.jar
 cp jpmml-sparkml-executable-1.6.0.jar spark/spark-3.0.0-preview2-bin-hadoop2.7/jars/
 
 # Install other packages
 sudo apt-get install bash;
 sudo apt-get install python3-pip;
-pip install pipenv;
-pipenv install
+sudo -H pip3 install -U pipenv;
+sudo pip3 install git+https://github.com/jpmml/pyspark2pmml.git;
+sudo pip3 install flask;
+sudo pip3 install pandas;
+sudo pip3 install numpy;
+sudo pip3 install findspark;
+sudo pip3 install pyspark;
+sudo pip3 install requests;
+sudo pip3 install pypmml;
+sudo pip3 install scikit-learn;
+sudo pip3 install scikit-multiflow;
+sudo pip3 install sklearn2pmml;
+sudo pip3 install sklearn-pandas;
+sudo pip3 install flask-cors;
 
 #Run the server
 bash run_analytics_server.sh
--- run_analytics_server.sh
+++ run_analytics_server.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 export FLASK_APP=./REST_Controllers/index.py
-source $(pipenv --venv)/bin/activate
 flask run -h 0.0.0.0
